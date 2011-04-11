afe11b43ab38aeba035f87dc218950bfa56afc22
Mladen Jablanovic <jablan@radioni.ca>
2011-04-11 18:33:00 +0200

    added sample output

--- examples/letter_count.rb
+++ examples/letter_count.rb
@@ -19,3 +19,4 @@ end
 manager = CollectInject::Manager.new(LetterCountWorker, 3)
 data = 'The quick brown fox jumps over the lazy dog'.chars
 p manager.run(data)
+#=> {"T"=>1, "u"=>2, "h"=>2, "c"=>1, "z"=>1, "j"=>1, "l"=>1, "x"=>1, "a"=>1, "w"=>1, "f"=>1, "m"=>1, "t"=>1, "d"=>1, "k"=>1, "r"=>2, "y"=>1, " "=>8, "i"=>1, "o"=>4, "s"=>1, "v"=>1, "e"=>3, "g"=>1, "q"=>1, "b"=>1, "n"=>1, "p"=>1}
